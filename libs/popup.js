4ef84245317beea680b81f70f7da2b79a3daa2f3
Hugo Rincon <hugorincon@MacHugo.local>
2013-09-07 15:07:44 -0430

    preview images on popup

--- libs/popup.js
+++ libs/popup.js
@@ -7,14 +7,53 @@ function copyTextToClipboard(text) {
   copyFrom.remove();
 }
 
+function div_for_word(word){
+  var div = document.createElement('div');
+  clip_image = chrome.extension.getURL("images/add.png")
+  div.innerHTML = "<span>:"+word+":</span><img class='clip' width='12' src='"+clip_image+"'/>";
+  div.className = 'word';
+  return div
+}
+
+function thumb_with_name(word){
+  div_image = document.createElement('div');
+  img_src = chrome.extension.getURL("images/"+word+".jpg");
+  div_image.innerHTML = "<img class='"+word+"' width='100%' src='"+ img_src +"'/>";
+  div_image.className = 'preview';
+  return div_image
+}
+
+function add_word(word){
+  item = div_for_word(word);
+  $(item).on('click', function(){
+    copyTextToClipboard(":"+word+":");
+  });
+  $('.content').append(item); 
+  $('.content').append("<div class='cl'></div>");
+}
+
+function add_image(word){
+  image = thumb_with_name(word);
+  $(image).on('click', function(){
+    expand_reduce_image_for(word);
+  }); 
+  $('.content').append(image);
+}
+
+function expand_reduce_image_for(word){
+  ima = $('img.'+word).parent();
+  if(ima.css('width') == '30px' || ima.css('width') == undefined){
+    ima.css('width', '200px');
+  }else{
+    ima.css('width', '30px');   
+  }
+}
+
 $(function(){
   chrome.storage.local.get('list', function (result) {
      _.each(result.list, function(word){ 
-       var div = document.createElement('div');
-       div.textContent = ":"+word+":";
-       div.className = 'word';
-       div.onclick = copyTextToClipboard(":"+word+":");
-       $('.content').append(div); 
+       add_image(word);
+       add_word(word);
      });   
   });
 });
